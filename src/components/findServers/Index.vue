24fbd626dda5f4d50f534d4d3a021cffb91727e1
Harol Restrepo <hstevdev@gmail.com>
2025-04-04 21:05:39 -0500

    feat: add the menu components

---
+++ src/components/findServers/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <Box propTitle="Find Servers">
+      <template #content>
+      </template>
+      <template #footer>
+        <Btn label="Cancel" />
+      </template>
+    </Box>
+</template>
+
+<script setup>
+import Box from '@/components/box/Index.vue'
+import Btn from '@/components/Btn.vue'
+</script>
---
+++ src/components/newGame/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <Box propTitle="New Game">
+      <template #content>
+      </template>
+      <template #footer>
+        <Btn label="Cancel" />
+      </template>
+    </Box>
+</template>
+
+<script setup>
+import Box from '@/components/box/Index.vue'
+import Btn from '@/components/Btn.vue'
+</script>
